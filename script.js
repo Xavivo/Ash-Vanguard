1940d0f193c207922010a89909a67d20ca13643b
AlbertoWebDev <albertovazquezmonge@gmail.com>
2025-11-04 18:23:00 +0100

    Troops deployment bug fixed

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+let numberOfResources = 300;
 
 // mouse
 
@@ -70,16 +72,61 @@ function handleGameGrid() {
 
 // projectiles
 //defenders
+class Defender {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.shooting = false;
+        this.health = 100;
+        this.projectiles = [];
+        this.timer = 0;
+    }
+    draw() {
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'gold';
+        ctx.font = '30px Arial';
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+    }
+}
+canvas.addEventListener('click', function() {
+    const gridPositionX = mouse.x - (mouse.x % cellSize);
+    const gridPositionY = mouse.y - (mouse.y % cellSize);
+    if (gridPositionY < cellSize) return;
+    for (let i = 0; i < defenders.length; i++) {
+        if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY)
+        return;
+    }
+    let defenderCost = 100;
+    if (numberOfResources >= defenderCost) {
+        defenders.push(new Defender(gridPositionX, gridPositionY));
+        numberOfResources -= defenderCost;
+    }
+})
+function handleDefenders() {
+    for (let i = 0; i < defenders.length; i++) {
+        defenders[i].draw();
+    }
+}
 //enemies
 //resources
 
 //utilities
+function handleGameStatus() {
+    ctx.fillStyle = 'gold';
+    ctx.font = '30px Arial';
+    fillText('Resources: ' + numberOfResources, 20, 55);
+}
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
     handleGameGrid();
+    handleDefenders();
+    handleGameStatus();
     requestAnimationFrame(animate);
 }
 
